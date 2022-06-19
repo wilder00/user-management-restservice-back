a7f66d1423e32bda82553274c205b0e278f24e6a
Wilder Trujillo <nadie437@gmail.com>
2022-06-19 00:59:00 -0500

    agregando config a sequelize, el dialecto (por error, corregido)

--- database/connection.js
+++ database/connection.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const DB_URL_CONNECTION = process.env.DB_URL_CONNECTION
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize(DB_URL_CONNECTION) // Example for postgres
+const sequelize = new Sequelize(DB_URL_CONNECTION,{
+  dialect: 'postgres'
+}) // Example for postgres
 
 module.exports = sequelize
